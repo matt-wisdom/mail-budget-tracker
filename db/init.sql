afa04b51cf892805e59c1e27e35ed6acf6cc8e1e
Matthew Wisdom <matthewwisdom11@gmail.com>
2025-02-02 19:53:32 +0100

    Update db model

--- db/init.sql
+++ db/init.sql
@@ -2,9 +2,19 @@ CREATE TABLE "dimdate" (
   "id" serial PRIMARY KEY,
   "date" timestamp,
   "year" integer,
-  "dow" integer,
+  "day_of_week" integer,
   "month" integer,
-  "dom" integer
+  "day_of_month" integer,
+  "hour" integer,
+  "minute" integer,
+  "second" integer
+);
+
+CREATE TABLE "dimcustomer" (
+  "id" serial PRIMARY KEY,
+  "sender" varchar(64),
+  "receiver" varchar(64),
+  "account_number" varchar(10)
 );
 
 CREATE TABLE "transactions" (
@@ -13,31 +23,26 @@ CREATE TABLE "transactions" (
   "date_id" integer,
   "user_id" integer,
   "bank_id" integer,
-  "transaction_type_id" integer
-);
-
-CREATE TABLE "dimuser" (
-  "id" serial PRIMARY KEY,
-  "username" varchar(32),
-  "location" varchar(64),
-  "created_at" timestamp
+  "transaction_id" integer
 );
 
 CREATE TABLE "dimbank" (
   "id" serial PRIMARY KEY,
-  "name" varchar(64),
-  "bank_class" varchar(10)
+  "bank_name" varchar(64)
 );
 
-CREATE TABLE "dim_transaction_type" (
+CREATE TABLE "dim_transaction" (
   "id" serial PRIMARY KEY,
-  "transaction_type" varchar(10)
+  "transaction_id" varchar(32),
+  "transaction_type" varchar(12),
+  "classification" varchar(12),
+  "description" varchar(128)
 );
 
 ALTER TABLE "transactions" ADD FOREIGN KEY ("bank_id") REFERENCES "dimbank" ("id");
 
-ALTER TABLE "transactions" ADD FOREIGN KEY ("transaction_type_id") REFERENCES "dim_transaction_type" ("id");
+ALTER TABLE "transactions" ADD FOREIGN KEY ("transaction_id") REFERENCES "dim_transaction" ("id");
 
 ALTER TABLE "transactions" ADD FOREIGN KEY ("date_id") REFERENCES "dimdate" ("id");
 
-ALTER TABLE "transactions" ADD FOREIGN KEY ("user_id") REFERENCES "dimuser" ("id");
+ALTER TABLE "transactions" ADD FOREIGN KEY ("user_id") REFERENCES "dimcustomer" ("id");
