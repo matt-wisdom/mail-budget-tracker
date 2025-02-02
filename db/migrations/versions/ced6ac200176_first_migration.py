6d4d6c353564b65cbaff9fc4e70ed3577d7267a2
Matthew Wisdom <matthewwisdom11@gmail.com>
2025-02-02 16:52:32 +0100

    Migration

---
+++ db/migrations/versions/ced6ac200176_first_migration.py
@@ -0,0 +1,24 @@
+"""first migration
+
+Revision ID: ced6ac200176
+Revises:
+Create Date: 2025-02-02 16:08:50.793349
+
+"""
+
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = "ced6ac200176"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
