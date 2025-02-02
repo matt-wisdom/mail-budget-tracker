b9221b7bf9c1555224ef2d96cbbd0bd9eb4322a1
Matthew Wisdom <matthewwisdom11@gmail.com>
2025-02-02 20:32:29 +0100

    Format

--- src/logger.py
+++ src/logger.py
@@ -8,15 +8,15 @@ LOGGER.setLevel(logging.DEBUG)
 
 # Create handlers
 console_handler = logging.StreamHandler()
-file_handler = logging.FileHandler('app.log')
+file_handler = logging.FileHandler("app.log")
 
 # Set log level for handlers
 console_handler.setLevel(logging.DEBUG)
 file_handler.setLevel(logging.WARNING)
 
 # Create formatters and add them to handlers
-console_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 console_handler.setFormatter(console_format)
 file_handler.setFormatter(file_format)
