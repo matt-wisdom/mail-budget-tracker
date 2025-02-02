093f304829fc5192750b0a947d1af4876219e832
Matthew Wisdom <matthewwisdom11@gmail.com>
2025-02-02 20:33:05 +0100

    Add transaction model

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -7,3 +8,17 @@ class EmailData(BaseModel):
     sender: str
     date_received: datetime
     body: str
+
+
+class Transaction(BaseModel):
+    amount: float
+    currency: str
+    date: datetime
+    bank_name: str
+    sender: Optional[str] = None
+    receiver: Optional[str] = None
+    transaction_type: str  # e.g., deposit, withdrawal, transfer, etc.
+    classification: str  # e.g., personal, business, salary, etc.
+    account_number: Optional[str] = None
+    transaction_id: Optional[str] = None
+    description: Optional[str] = None
